0f4e2c7f8749ddfd97bd1a9ca7e6c3f8ee42a14a
Sam <s.kamber8@gmail.com>
2015-01-16 11:56:05 +0000

    Cleans up deh codez here and there.

--- SOCVR.Net/SOCVR.Net/Question.cs
+++ SOCVR.Net/SOCVR.Net/Question.cs
@@ -1,55 +1,54 @@
-﻿//using System.Collections.Generic;
-//using System.Net;
-//using System.Text;
-//using CsQuery;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using CsQuery;
 
 
 
-//namespace SOCVRDotNet
-//{
-//    public class Question :Post
-//    {
-//        public List<string> Tags { get; private set; }
-//        public string Html { get; private set; }
+namespace SOCVRDotNet
+{
+    public class Question : Post
+    {
+        public List<string> Tags { get; private set; }
+        public string Html { get; private set; }
 
 
 
-//        public Question(string url, string title, string site, string authorName, string authorLink, List<string> tags)
-//        {
-//            Url = url;
-//            Title = title;
-//            Site = site;
-//            AuthorName = authorName;
-//            AuthorLink = authorLink;
-//            Tags = tags;
-                
-//            PopulateExtraData();
-            
-//        }
+        public Question(string url, string title, string site, string authorName, string authorLink, List<string> tags)
+        {
+            Url = url;
+            Title = title;
+            Site = site;
+            AuthorName = authorName;
+            AuthorLink = authorLink;
+            Tags = tags;
 
-//        public Question(string url, string title, string body, string site, int score, string authorName, string authorLink, int authorRep, List<string> tags)
-//        {
-//            Url = url;
-//            Title = title;
-//            Body = body;
-//            Site = site;
-//            Score = score;
-//            AuthorName = authorName;
-//            AuthorLink = authorLink;
-//            AuthorRep = authorRep;
-//            Tags = tags;
-//        }
+            PopulateExtraData();
+        }
 
+        public Question(string url, string title, string body, string site, int score, string authorName, string authorLink, int authorRep, List<string> tags)
+        {
+            Url = url;
+            Title = title;
+            Body = body;
+            Site = site;
+            Score = score;
+            AuthorName = authorName;
+            AuthorLink = authorLink;
+            AuthorRep = authorRep;
+            Tags = tags;
+        }
 
 
-//        private void PopulateExtraData()
-//        {
-//           Html = new WebClient{ Encoding = Encoding.UTF8 }.DownloadString(Url);
-//            var dom = CQ.Create(Html);
 
-//            Body = WebUtility.HtmlDecode(dom[".post-text"].Html().Trim());
-//            Score = int.Parse(dom[".vote-count-post"].Html());
-//            AuthorRep = PostRetriever.ParseRep(dom[".reputation-score"].Html());
-//        }
-//    }
-//}
+        private void PopulateExtraData()
+        {
+            Html = new WebClient { Encoding = Encoding.UTF8 }.DownloadString(Url);
+            var dom = CQ.Create(Html);
+
+            Body = WebUtility.HtmlDecode(dom[".post-text"].Html().Trim());
+            Score = int.Parse(dom[".vote-count-post"].Html());
+            AuthorRep = PostRetriever.ParseRep(dom[".reputation-score"].Html());
+        }
+    }
+}
