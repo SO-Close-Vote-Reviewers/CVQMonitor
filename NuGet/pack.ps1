31f58994adc120a12a72123e7535acc87661eb13
Sam <ArcticEcho@users.noreply.github.com>
2015-07-09 14:14:45 +0100

    Automate deployment using AppVeyor

---
+++ NuGet/pack.ps1
@@ -0,0 +1,12 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\SOCVR.Net\bin\Release\SOCVR.Net.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}-beta" -f ($version.Major, $version.Minor, $version.Revision)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\nuget\SOCVR.Net.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\nuget\SOCVR.Net.compiled.nuspec
+
+& $root\nuget\NuGet.exe pack $root\nuget\SOCVR.Net.compiled.nuspec
