a5f3202bf15c510d57e1ab095e0a6fb95365d437
Sam <ArcticEcho@users.noreply.github.com>
2016-07-16 23:20:05 +0100

    Check if review was an audit + fetch review's tags

--- CVQMonitor/Review.fs
+++ CVQMonitor/Review.fs
@@ -1,16 +1,17 @@
 ﻿namespace CVQMonitor
 
 open System
+open System.Collections.Generic
 open RestSharp
 
 type Review (reviewID : int, reviewerID : int) =
     let reviewBaseUrl = "http://stackoverflow.com/review/next-task/"
-    
+
     let mutable reviewerName = ""
     let mutable timestamp = DateTime.MinValue
-    let mutable tags = [""]
     let mutable action = ReviewAction.Close
     let mutable auditPassed = new Nullable<bool> ()
+    let mutable tags = new List<string> ()
 
     do
         let reviewUrl = reviewBaseUrl + reviewID.ToString ()
@@ -19,11 +20,14 @@ type Review (reviewID : int, reviewerID : int) =
         req.AddParameter ("fkey", FkeyCollector.Fkey) |> ignore
         let res = RequestScheduler.ProcessRequest req
         if String.IsNullOrWhiteSpace res.Content |> not then
-            let html = Json.GetField res.Content "instructions" |> Json.EscapeData
-            let (name, time, act) = ReviewParser.GetReviewResultData html reviewerID
+            let resultsHtml = Json.GetField res.Content "instructions" |> Json.EscapeData
+            let postHtml = Json.GetField res.Content "content" |> Json.EscapeData
+            let (name, time, act) = ReviewParser.GetReviewResultData resultsHtml reviewerID
             reviewerName <- name
             timestamp <- time
             action <- act
+            auditPassed <- ReviewParser.AuditPassed resultsHtml
+            tags <- ReviewParser.GetPostTags postHtml
         ()
 
     member this.ID = reviewID
@@ -34,8 +38,8 @@ type Review (reviewID : int, reviewerID : int) =
 
     member this.Timestamp = timestamp
 
-    member this.Tags = tags
-
     member this.Action = action
 
     member this.AuditPassed = auditPassed
+
+    member this.Tags = tags
--- CVQMonitor/ReviewParser.fs
+++ CVQMonitor/ReviewParser.fs
@@ -1,9 +1,13 @@
 ﻿module internal CVQMonitor.ReviewParser
 
 open System
+open System.Collections.Generic
 open System.Text.RegularExpressions
 
+let private regOpts = RegexOptions.Compiled ||| RegexOptions.CultureInvariant
 let private resultDataBasePtn = """(?is)<div\s*?class="review-results"\s*?>\s*?<a\s*?href="/users/$USERID$/.*?">(.*?)</a>.*?<span\s*?title="(.*?)".*?<b>([a-z\s]+)</b>\s*?</div>"""
+let private auditPassedReg = new Regex ("""(?is)<strong>\s*?review audit (passed|failed)""", regOpts)
+let private tagsReg = new Regex ("""(?is)<div\s*?class="post-taglist">(\s*?<a\s*?href="/questions/tagged/\S+?".*?>(\S+?)</a>)+""", regOpts)
 
 let GetReviewResultData html userID =
     let ptn = resultDataBasePtn.Replace ("$USERID$", userID.ToString ())
@@ -21,5 +25,17 @@ let GetReviewResultData html userID =
             | _ -> ReviewAction.Null
         (name, timestamp, action)
 
-let PassedAudit html =
-    true
+let AuditPassed html =
+    let m = auditPassedReg.Match html
+    if not m.Success then
+        new Nullable<bool> ()
+    else
+        new Nullable<bool> (m.Groups.[1].Value = "passed")
+
+let GetPostTags html =
+    let m = tagsReg.Match html
+    let tags = new List<string> ()
+    if m.Success then
+        for (t : Capture) in m.Groups.[2].Captures do
+            tags.Add t.Value
+    tags
