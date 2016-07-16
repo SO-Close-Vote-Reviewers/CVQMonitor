592cfe3f49e4e7fae236de745b58f62f7e699e96
Sam <ArcticEcho@users.noreply.github.com>
2016-07-16 22:31:03 +0100

    Correct timestamp parsing + parse review action

--- CVQMonitor/ReviewParser.fs
+++ CVQMonitor/ReviewParser.fs
@@ -3,7 +3,7 @@
 open System
 open System.Text.RegularExpressions
 
-let private resultDataBasePtn = """(?is)<div\s*?class="review-results"\s*?>\s*?<a\s*?href="/users/$USERID$/.*?">(.*?)</a>.*?<span\s*?title="(.*?)".*?</div>"""
+let private resultDataBasePtn = """(?is)<div\s*?class="review-results"\s*?>\s*?<a\s*?href="/users/$USERID$/.*?">(.*?)</a>.*?<span\s*?title="(.*?)".*?<b>([a-z\s]+)</b>\s*?</div>"""
 
 let GetReviewResultData html userID =
     let ptn = resultDataBasePtn.Replace ("$USERID$", userID.ToString ())
@@ -13,5 +13,13 @@ let GetReviewResultData html userID =
     else
         let name = m.Groups.[1].Value
         let timestampStr = m.Groups.[2].Value
-        let timestamp = DateTime.Parse timestampStr
-        (name, timestamp, ReviewAction.Close)
+        let timestamp = (DateTime.Parse timestampStr).ToUniversalTime ()
+        let action =
+            match m.Groups.[3].Value.ToLowerInvariant () with
+            | "close" -> ReviewAction.Close
+            | "leave open" -> ReviewAction.LeaveOpen
+            | _ -> ReviewAction.Null
+        (name, timestamp, action)
+
+let PassedAudit html =
+    true
