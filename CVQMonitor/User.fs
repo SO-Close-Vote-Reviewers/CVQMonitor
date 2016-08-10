a5fea39fca216db9c9db0c6405be805fc94fc61a
Sam <ArcticEcho@users.noreply.github.com>
2016-08-10 17:05:35 +0100

    Hopefully fix audit bug

--- CVQMonitor/User.fs
+++ CVQMonitor/User.fs
@@ -7,25 +7,40 @@ open System.Threading.Tasks
 type User (userID : int) as this =
     //TODO: Ideally we should be fetching the review limit, not hardcoding it in.
     let reviewLimit = 40
+    let itemReviewedEv = new Event<User * Review> ()
+    let reviewingStartedEv = new Event<User> ()
+    let reviewingLimitReachedEv = new Event<User> ()
+    let _revStartedEv = new Event<unit> ()
+    let _revStarted = _revStartedEv.Publish
     let mutable dispose = false
     let mutable isReviewing = false
     let mutable lastReviewTime = DateTime.MinValue
     let mutable reviewCache : Review list = []
     let mutable trueReviewCount = 0
     let mutable isMod = false
-    let itemReviewedEv = new Event<User * Review> ()
-    let reviewingStartedEv = new Event<User> ()
-    let reviewingLimitReachedEv = new Event<User> ()
+
+    let calculateSelectiveAvgSecs () : float =
+        match reviewCache.Length with
+        | x when x = 0 || x = 1 -> 15.0
+        | _ ->
+            let mutable avg = 0.0
+            for i in 0 .. reviewCache.Length - 2 do
+                let diff =
+                    reviewCache.[i].Timestamp
+                    |> (-) reviewCache.[i + 1].Timestamp
+                let diffSecs = diff.TotalSeconds
+                avg <-
+                    match diffSecs with
+                    | x when x > 180.0 -> avg
+                    | _ -> (avg + diffSecs) / 2.0
+            avg
 
     let checkLimitReached () =
-        if not isMod && (trueReviewCount >= reviewLimit || reviewCache.Length >= reviewLimit) then
-            reviewingLimitReachedEv.Trigger this
+        if not isMod && isReviewing && (trueReviewCount >= reviewLimit || reviewCache.Length >= reviewLimit) then
             isReviewing <- false
+            reviewingLimitReachedEv.Trigger this
 
     let handleNonAuditReviewed () =
-        isReviewing <- true
-        if lastReviewTime.Date <> DateTime.UtcNow.Date then
-            reviewingStartedEv.Trigger this
         let revsToCheck =
             UserProfileScraper.GetReviewsByPage userID 1
             |> Seq.filter (fun x ->
@@ -43,14 +58,23 @@ type User (userID : int) as this =
                 lastReviewTime <- review.Timestamp
         checkLimitReached ()
 
+    let checkForAudits () =
+        while isReviewing && not dispose do
+            let mutable wait =
+                let secs =  calculateSelectiveAvgSecs () / 3.0 * 2.0
+                let safeSecs = Math.Max (Math.Min (secs, 60.0), 15.0)
+                TimeSpan.FromSeconds safeSecs
+            Thread.Sleep wait
+            handleNonAuditReviewed ()
+
     let reviewCountUpdater () =
         while not dispose do
             Thread.Sleep 15000
             if isReviewing then
                 trueReviewCount <- UserProfileScraper.GetTodayReviewCount userID
+                checkLimitReached ()
                 if DateTime.UtcNow - lastReviewTime > TimeSpan.FromMinutes 3.0 then
                     isReviewing <- false
-                checkLimitReached ()
 
     let dailyReset () =
         while not dispose do
@@ -66,7 +90,15 @@ type User (userID : int) as this =
         for rev in revsToday do
             let review = new Review (fst rev, userID)
             reviewCache <- review :: reviewCache
-        CVQActivityMonitor.NonAuditReviewed.Add (fun id -> if id = userID then handleNonAuditReviewed ())
+        CVQActivityMonitor.NonAuditReviewed.Add (fun id ->
+            if id = userID then
+                isReviewing <- true
+                if lastReviewTime.Date <> DateTime.UtcNow.Date then
+                    reviewingStartedEv.Trigger this
+                handleNonAuditReviewed ()
+                _revStartedEv.Trigger ()
+        )
+        _revStarted.Add (fun () -> checkForAudits ())
         Task.Run reviewCountUpdater |> ignore
         Task.Run dailyReset |> ignore
 
@@ -91,4 +123,5 @@ type User (userID : int) as this =
         member this.Dispose () =
             if not dispose then
                 dispose <- true
+                isReviewing <- false
                 GC.SuppressFinalize this
