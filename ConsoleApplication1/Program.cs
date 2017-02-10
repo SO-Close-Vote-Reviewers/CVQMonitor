0ebb29404eb38f8907060ae39a5247d33f9e6008
Sam <ArcticEcho@users.noreply.github.com>
2017-02-10 20:01:23 +0000

    Just gonna add this real quick

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CVQMonitor;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User(2246344);
+
+            user.ReviewingStarted += (o, e) => Console.WriteLine("Started.");
+            user.ReviewLimitReached += (o, e) => Console.WriteLine("Finished.");
+            user.ItemReviewed += (o, e) => Console.WriteLine($"{e.Item2.ID} reviewed.");
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+    }
+}
