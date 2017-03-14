cc2c35ff36a11017fa9ba7e9f39f3b9f84bebb79
Sam <ArcticEcho@users.noreply.github.com>
2017-03-14 18:47:30 +0000

    no need for chat stuff

--- Test/Program.cs
+++ Test/Program.cs
@@ -12,59 +12,16 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            var client = new ChatExchangeDotNet.Client("", "");
-            var room = client.JoinRoom("https://chat.stackoverflow.com/rooms/68414/socvr-testing-facility", true);
+            RequestScheduler.RequestsPerMinute = 30;
 
-            Console.WriteLine("Joined chat.");
+            var user1 = new User(2246344); // Me
+            var user2 = new User(3956566); // Yvette Colomb
+            var user3 = new User(2756409); // TylerH
 
-            Task.Run(() =>
+            while (true)
             {
-                try
-                {
-                    var user1 = new User(2246344); // Me
-                    var user2 = new User(3956566); // Yvette Colomb
-                    var user3 = new User(2756409); // TylerH
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
-
-                while (true)
-                {
-                    Thread.Sleep(1000);
-                }
-            });
-
-            var lastLine = "";
-
-            using (var strm = new StringWriter())
-            {
-                Console.SetOut(strm);
-                while (true)
-                {
-                    Thread.Sleep(5000);
-                    var lines = strm.ToString().Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-
-                    var msg = "";
-                    foreach (var line in lines)
-                    {
-                        if (!lastLine.StartsWith(":") || !line.StartsWith(":") || !line.Split(':')[1].All(Char.IsDigit) || int.Parse(lastLine.Split(':')[1]) < int.Parse(line.Split(':')[1]))
-                        {
-                            if (line.StartsWith(":"))
-                            {
-                                lastLine = line;
-                            }
-                            msg += line + "\n";
-                        }
-                    }
-                    if (!string.IsNullOrWhiteSpace(msg))
-                    {
-                        room.PostMessageLight(msg);
-                    }
-                }
+                Thread.Sleep(1000);
             }
-            
         }
     }
 }
